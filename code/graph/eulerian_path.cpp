b0869823727587d2387a03bfcee50ac59e1216c5
Simon Fallon <sfallong@eafit.edu.co>
2026-06-03 22:57:09 -0500

    fix eulerian_path, use lowercase for graph filenames

--- code/graph/eulerian_path.cpp
+++ code/graph/eulerian_path.cpp
@@ -4,53 +4,67 @@ int n;
 int edges = 0;
 int out[nax], in[nax];
 
-// Directed version (uncomment commented code for undirected)
 struct edge {
-  int v;
-//  list<edge>::iterator rev;
-  edge(int v):v(v){}
+  int v, id;
+//  list<edge>::iterator rev;          
+  edge(int v, int id):v(v),id(id){}
 };
 list<edge> g[nax];
-void add_edge(int a, int b){
+void add_edge(int a, int b, int id){
   out[a]++;
   in[b]++;
   ++edges;
-  g[a].push_front(edge(b));//auto ia=g[a].begin();
-//  g[b].push_front(edge(a));auto ib=g[b].begin();
-//  ia->rev=ib;ib->rev=ia;
+  g[a].push_front(edge(b,id)); //auto ia=g[a].begin();
+//  g[b].push_front(edge(a,id)); auto ib=g[b].begin();
+//  ia->rev=ib; ib->rev=ia;                          
 }
-vi p;
+vi p, pe; // pe = edge ids (judge wants edge indices, not nodes)
 void go(int u){
   while(sz(g[u])){
     int v=g[u].front().v;
-    //g[v].erase(g[u].front().rev);
+    int id=g[u].front().id;
+//    g[v].erase(g[u].front().rev);
     g[u].pop_front();
     go(v);
+    pe.push_back(id);
   }
   p.push_back(u);
 }
-
-vi get_path(int u){   
-  p.clear();
+vi get_path(int u){
+  p.clear(); pe.clear();
   go(u);
-  reverse(all(p));
+  reverse(all(p)); reverse(all(pe));
   return p;
 }
 
-/// for undirected uncomment and check for path existance
-bool eulerian(vi &tour) { /// directed graph
-  int one_in = 0, one_out = 0, start = -1;
-  bool ok = true;
-  for (int i = 0; i < n; i++) {
-    if(out[i] && start == -1) start = i;
-    if(out[i] - in[i] == 1) one_out++, start = i;
-    else if(in[i] - out[i] == 1) one_in++;
-    else ok &= in[i] == out[i];
+pair<bool, int> check_directed() {
+  int start = -1, plus = 0, minus = 0, bad = 0;
+  forn(i, n){
+    if (out[i] && start == -1) start = i;
+    int d = out[i] - in[i];
+    if (d == 1){ plus++; start = i; }
+    else if (d == -1) minus++;
+    else if (d) bad++;
   }
-  ok &= one_in == one_out && one_in <= 1;
-  if (ok) {
+  return {(bad == 0 && plus == minus && plus <= 1), start};
+}
+
+pair<bool, int> check_undirected() {
+  int start = -1, odd = 0;
+  forn(i, n){
+    int deg = out[i] + in[i];
+    if (deg && start == -1) start = i;
+    if (deg & 1) { odd++; start = i; }
+  }
+  return {(odd == 0 || odd == 2), start};
+}
+
+// For undirected -> Use check_undirected, uncomment lines above
+bool eulerian(vi& tour){
+  auto [ok, start] = check_directed();
+  if (ok && start != -1) {
     tour = get_path(start);
-    if(sz(tour) == edges + 1) return true;
-  }
+    if (sz(tour) == edges + 1) return true; 
+  } // Remove + 1 if pe is returned (edge indices)
   return false;
 }
